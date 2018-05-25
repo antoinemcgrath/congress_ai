7f21577ac3845ebb62c98c28bfe1e6972a70130a
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-05-24 23:31:22 -0700

    update

---
+++ db_congressai/download_urls.py
@@ -0,0 +1,27 @@
+'''
+Creates in URLs Collection
+
+Dowloads
+  IAold            -epoch
+  sha_IAold        -sha
+  IAnew            -epoch
+  sha_IAnew        -sha
+  IAcall           -epoch
+  sha_IAcall       -sha
+  Origin           -epoch
+  sha_Origin       -sha
+PDF                -1/0
+Retire             -1/0
+
+
+Creates in Files Collection
+id                 -obj_id
+Date_Added         -epoch
+URL_Sources        -list
+Sha256             -str
+Filetype_file
+Filetype_mime
+Filename           -list
+PDF_Unlock          -binary
+'''
+
